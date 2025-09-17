23619d5c50d28bc339d0092c6d607b0ebff212b4
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-09-17 17:47:16 +0000

    area & perimeter of a 7x4 rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Brandon
+# Date: September 12th, 2025
+# This program displays the area and perimeter of a rectangle that has a length of 7cm and a width of 4cm
+
+
+def main():
+    print("For a rectangle with the length of 7cm and the width of 4cm:")
+    print()
+    print("The area is: {}cm^2".format(7 * 4))
+    print("The perimeter is: {}cm".format(2 * (7 + 4)))
+
+
+if __name__ == "__main__":
+    main()
